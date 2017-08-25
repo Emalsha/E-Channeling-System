d6364ae403e2ba09127c29b73f99a2c36552af7d
G.H.B.Emalsha Rasad <remalsha@gmail.com>
2017-08-25 10:26:15 +0530

    Start form

--- Channeling System/DoctorPanel/LoginForm.Designer.cs
+++ Channeling System/DoctorPanel/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DoctorPanel
 {
-    partial class start
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,42 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(365, 289);
-            this.panel1.TabIndex = 0;
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::DoctorPanel.Properties.Resources.ACMA_logo_2;
-            this.pictureBox1.Location = new System.Drawing.Point(74, 16);
+            this.pictureBox1.Image = global::DoctorPanel.Properties.Resources.ACMA_logo_1;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 113);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(216, 200);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(225, 70);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // start
+            // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(391, 313);
-            this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "start";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.start_Load);
-            this.panel1.ResumeLayout(false);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -71,9 +56,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-
     }
 }
-
