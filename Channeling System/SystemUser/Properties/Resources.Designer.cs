d5b5c4db68b419e56428fda57c5ab5e55cc24abc
Delushaan <delushaan@outlook.com>
2017-08-25 10:57:01 +0530

    Start Form UI completed

--- Channeling System/SystemUser/Properties/Resources.Designer.cs
+++ Channeling System/SystemUser/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace SystemUser.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ACMA_logo_2 {
+            get {
+                object obj = ResourceManager.GetObject("ACMA-logo-2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hospital {
             get {
                 object obj = ResourceManager.GetObject("hospital", resourceCulture);
